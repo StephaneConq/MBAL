81ee450e82dc6b240a62eb1b8df4ad985542a0ee
Unknown <conqstephane@gmail.com>
2018-02-28 11:14:27 +0100

    front

--- app/app.js
+++ app/app.js
@@ -2,13 +2,45 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+
+    .controller('ctrl', function($scope){
+
+        var ctrl = this;
+
+        ctrl.login = "Connexion";
+
+        $('.login').on('click', function(event) {
+
+            var target = $(this.getAttribute('href'));
+
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            }
+
+        });
+
+        materializeInit();
+
+        function materializeInit(){
+            $('.carousel.carousel-slider').carousel({
+                fullWidth: true,
+                indicators: true
+            });
+        }
+
+
+    });
